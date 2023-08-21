937ecc32ecc20b718a592bef04249dccc51ed6ab
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2023-08-21 20:29:16 -0300

    feat: added testing for the sum function

--- calculator.py
+++ calculator.py
@@ -6,3 +6,9 @@ def intSum (a, b):
         print("One of the parameters is not on a valid integer format...")
     else:
         return a + b
+
+print(intSum(4, 5)) # Should return 9.0
+print(intSum(4.5, 5)) # Should return 9.5
+print(intSum(4, 'a')) # Should launch an error
+print(intSum(4, False)) # Should return 4.0
+print(intSum(4, True)) # Should return 5.0
