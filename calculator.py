4123ab9b0585b13e07d7e0d11712a6c558d82021
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2023-08-21 20:32:56 -0300

    feat: added subtraction function

--- calculator.py
+++ calculator.py
@@ -3,12 +3,25 @@ def intSum (a, b):
     b = float(b)
 
     if (type(a) != float or type(b) != float):
-        print("One of the parameters is not on a valid integer format...")
+        print("One of the parameters is not on a valid numerical format...")
     else:
         return a + b
 
+"""
+# Sum function tests:
 print(intSum(4, 5)) # Should return 9.0
 print(intSum(4.5, 5)) # Should return 9.5
 print(intSum(4, 'a')) # Should launch an error
 print(intSum(4, False)) # Should return 4.0
 print(intSum(4, True)) # Should return 5.0
+"""
+
+def subtract (a, b):
+    a = float(a)
+    b = float(b)
+
+    if (type(a) != float or type(b) != float):
+        print("One of the parameters is not on a valid numerical format...")
+    else:
+        return a - b
+
