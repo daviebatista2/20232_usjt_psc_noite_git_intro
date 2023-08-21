53dd89257c8f76f4164bec6d45a0738db587ce0e
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2023-08-21 20:37:53 -0300

    feat: added multiply function

--- calculator.py
+++ calculator.py
@@ -25,3 +25,20 @@ def subtract (a, b):
     else:
         return a - b
 
+"""
+# Subtraction function tests:
+print(subtract(5, 4)) # Should return 1.0
+print(subtract(4.5, 5)) # Should return -0.5
+print(subtract(4, 'a')) # Should launch an error
+print(subtract(4, False)) # Should return 4.0
+print(subtract(4, True)) # Should return 3.0
+"""
+
+def multiply (a, b):
+    a = float(a)
+    b = float(b)
+
+    if (type(a) != float or type(b) != float):
+        print("One of the parameters is not on a valid numerical format...")
+    else:
+        return a * b
